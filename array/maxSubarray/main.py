a61d286f069b90c23ce12a900c637e963498fba6
Sarvesh Hardas <hardassarvesh58@gmail.com>
2026-01-01 02:23:33 +0530

    max Subarray and sort color

--- array/maxSubarray/main.py
+++ array/maxSubarray/main.py
@@ -5,9 +5,15 @@ with open("input.txt","r") as f:
 
 # solution starts here
 
+current_sum = input[0]
+best_sum = input[0]
+
+for num in input[1:]:
+    current_sum = max(num,current_sum + num)
+    best_sum = max(best_sum,current_sum)
 
 
 # solution ends here 
 
 with open("output.txt","a") as f:
-    f.write(str(input))
+    f.write(str(best_sum))
